37f385617b1822b6306eb4f336749f1ebe4905f9
ezreal <ezreal.ice@icloud.com>
2023-07-27 12:10:32 +0100

    html

---
+++ template-parts/index/Ad-Blocker.php
@@ -0,0 +1,32 @@
+<section class="mb-12" id="content4">
+      <div class="flex justify-between headline mb-4 items-center">
+        <div class="title">
+          Ad Blocker
+          <span class="badge">Total 10 articles</span>
+        </div>
+        <button class="operation">
+          View All
+        </button>
+      </div>
+      <div class="flex items-center justify-evenly is-grid-poster-wall feature-article">
+
+        <div class="bgImgSrc1 bgImg flex flex-col">
+          <p class="article_tag mb-4">Ad Blocker</p>
+          <p class="article_title grow">How to Block Ads on Instagram with ZimaBoard Single Board Server</p>
+          <p class="flex article_info items-center shrink">
+            <img class="w-5 h-5 mr-2" src="./fonts/zima/logo-dark.svg" />
+            ZimaBoard - 01/06/2023
+          </p>
+        </div>
+
+        <div class="bgImgSrc2 bgImg flex flex-col">
+          <p class="article_tag mb-4">Ad Blocker</p>
+          <p class="article_title grow">How to Block Ads on Facebook with ZimaBoard Personal Server</p>
+          <p class="flex article_info items-center shrink">
+            <img class="w-5 h-5 mr-2" src="./fonts/zima/logo-dark.svg" />
+            ZimaBoard - 01/06/2023
+          </p>
+        </div>
+
+      </div>
+    </section>
---
+++ template-parts/index/Latest-News.php
@@ -0,0 +1,46 @@
+<section class="mb-12 slideshow-article" id="content1">
+      <div class="flex justify-between headline mb-4 items-center">
+        <div class="title grow">
+          Latest News
+        </div>
+        <button class="swiper-button-prev shrink w-10 h-10">
+          <img src="fonts/icon/swiper-button-prev.svg">
+        </button>
+        <button class="swiper-button-next shrink w-10 h-10 ml-4">
+          <img src="fonts/icon/swiper-button-next.svg">
+        </button>
+      </div>
+
+      <!-- Slider main container -->
+      <div class="swiper">
+        <!-- Additional required wrapper -->
+        <div class="swiper-wrapper">
+          <!-- Slides Start -->
+          <div class="swiper-slide bgImgSrc1 bgImg">
+            <div class="flex flex-col justify-end bgMask">
+              <p class="article_tag">Ad Blocker</p>
+              <p class="article_title my-2">ZimaBoard Prime Discount Day -14th Of Each Month</p>
+              <p class="flex article_info items-center">
+                <img class="w-5 h-5 mr-2" src="./fonts/zima/logo-dark.svg" />
+                ZimaBoard - 01/06/2023
+              </p>
+            </div>
+          </div>
+
+          <div class="swiper-slide bgImgSrc1 bgImg">
+            <div class="flex flex-col justify-end bgMask">
+              <p class="article_tag">Ad Blocker</p>
+              <p class="article_title my-2">ZimaBoard Prime Discount Day -14th Of Each Month</p>
+              <p class="flex article_info items-center">
+                <img class="w-5 h-5 mr-2" src="./fonts/zima/logo-dark.svg" />
+                ZimaBoard - 01/06/2023
+              </p>
+            </div>
+          </div>
+          <!-- Slides End -->
+        </div>
+
+        <div class="swiper-pagination"></div>
+      </div>
+
+    </section>
